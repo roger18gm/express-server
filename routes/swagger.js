e6408e9e4468ab0911eced0febcdce8f980b789b
rawwyurr <roger18gm@gmail.com>
2025-05-24 23:20:08 -0600

    update CORS middleware

--- routes/swagger.js
+++ routes/swagger.js
@@ -5,7 +5,6 @@ import { readFileSync } from 'fs';
 const router = express.Router();
 const swaggerDocument = JSON.parse(readFileSync(new URL('../swagger-output.json', import.meta.url)));
 
-router.use("/", swaggerUi.serve);
-router.get("/", swaggerUi.setup(swaggerDocument));
+router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default router;
